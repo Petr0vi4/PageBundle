aab1e88f7d52ffb5f6e5aa13455d5d0722075283
Pavel B <p.bondarovich@gmail.com>
2017-01-30 17:28:34 +0700

    fix meta keywords and description

--- CreonitPage.php
+++ CreonitPage.php
@@ -168,8 +168,8 @@ class CreonitPage
         return "
             <title>{$title}</title>
             <meta name=\"title\" content=\"{$title}\" />
-            <meta name=\"keywords\" content=\"{$description}\" />
-            <meta name=\"description\" content=\"{$keywords}\" />
+            <meta name=\"keywords\" content=\"{$keywords}\" />
+            <meta name=\"description\" content=\"{$description}\" />
         ";
 
     }
